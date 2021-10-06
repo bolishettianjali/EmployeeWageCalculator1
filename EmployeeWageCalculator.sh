bf87da6118937a8d89346731b169319077b82bef
bolishettianjali <bolishettianjali@gamil.com>
2021-10-06 20:31:09 +0530

    Employee is present or abscent

--- EmployeeWageCalculator.sh
+++ EmployeeWageCalculator.sh
@@ -1,3 +1,16 @@
-#!bin/bash
+#!/bin/bash
 
 echo "Welcome to Employee Wage Calculator"
+
+IS_PRESENT=1
+
+empCheck=$((RANDOM%2))
+
+if [ $empCheck -eq $IS_PRESENT ]
+then
+	
+	echo "Employee is present"
+
+else
+	echo "Employee is absent"
+fi
